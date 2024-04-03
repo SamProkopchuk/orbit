62e4bc72b511c84b6f8dc2e82b8f7723fe386bc5
Sam Prokopchuk <ssprokopchuk@gmail.com>
2024-04-03 14:32:13 -0400

    Minor change

--- source/standalone/workflows/skrl/train.py
+++ source/standalone/workflows/skrl/train.py
@@ -216,7 +216,7 @@ def main():
     # Set main camera
     sim.set_camera_view(eye=[3.5, 3.5, 3.5], target=[0.0, 0.0, 0.0])
     # design scene
-    scene_cfg = SensorsSceneCfg(num_envs=args_cli.num_envs, env_spacing=2.0)
+    scene_cfg = SensorsSceneCfg(num_envs=env.num_envs, env_spacing=2.0)
     scene = InteractiveScene(scene_cfg)
     # Play the simulator
     sim.reset()
